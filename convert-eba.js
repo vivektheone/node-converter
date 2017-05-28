3f3a2ff6c7566e775b5fac392a5bd00cdfa21ef1
Vivek VK <vivektheone@gmail.com>
2017-05-28 22:58:03 +0530

    Added conversion

---
+++ convert-eba.js
@@ -0,0 +1,32 @@
+const csvFilePath='input.csv'
+const csv=require('csvtojson')
+var fastCsv = require('fast-csv')
+const lodash = require('lodash')
+var fs = require('fs')
+var datas = [];
+csv({
+  noheader:false,
+  headers: ['one', 'two', 'three']
+})
+.fromFile(csvFilePath)
+.on('json',(row)=>{ // this func will be called 3 times 
+    datas.push(row);
+})
+.on('done',()=>{
+   console.log(datas);
+   var csvStream = fastCsv.createWriteStream({headers: true}),
+   writableStream = fs.createWriteStream("output.csv");
+   csvStream.pipe(writableStream);
+   lodash.forEach(datas, function(value) {
+     csvStream.write(value);
+     console.log(value);
+   });
+   
+   writableStream.on("finish", function(){
+     console.log("DONE!");
+   });
+ 
+   csvStream.pipe(writableStream);
+   csvStream.end();
+
+});
